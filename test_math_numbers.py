266a89c9b63cf3aef959b15362ad36b23df1cea2
Chan Basani <chandradhar.basani@gmail.com>
2025-09-11 20:27:09 +0200

    defined functions to math

--- test_math_numbers.py
+++ test_math_numbers.py
@@ -1,12 +1,38 @@
-a = 55
-b = 11
-w = a / b
-
-print("w is equal to a =%d / b=%d ",a,b,w)
-
-x = 20 - 3
-y = 6 * 7
-z = 3 + 12
+def addition():
+    a = 3
+    b = 12
+    c = a + b
+    print("%s plus %d equals to %f." % (a,b,c))
 
 
-print(w, x, y, z)
+def substraction():
+    a = 20
+    b = 8
+    c = a - b
+    print("%s minus %d equals to %f." % (a,b,c))
+
+
+def multiplication():
+    a = 6
+    b = 7
+    c = a * b
+    print("%s multiplied by %d equals to %f." % (a,b,c))
+
+
+def division():
+    a = 55
+    b = 11
+    c = a / b
+    print("%s divided by %d equals to %f." % (a,b,c))
+
+
+def main():
+    print("We are going to calculate different things ... ")
+    addition()
+    substraction()
+    multiplication()
+    division()
+    print("This is it..")
+
+
+main()
